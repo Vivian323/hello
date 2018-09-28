1af1a50597630ae16979a75b4df5f82974b528bc
Vivian Zhang <zvivian36@gmail.com>
2018-09-28 12:38:34 -0700

    introduce Vivian

--- pset0.R
+++ pset0.R
@@ -1,2 +1,2 @@
 print("Hello World")
-print ("Hello! This is Vivian Zhang who just graduated from UCSD and now a research coordinator in Stanford")
+print ("Hello! This is Vivian Zhang who just graduated from UCSD ")
